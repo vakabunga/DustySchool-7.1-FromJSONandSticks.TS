1a04a678dd8d89229a7acd3a5affb9d84f906aff
Vaka.pro <vakabunga@proton.me>
2025-07-25 13:48:41 +0400

    refactor: creates frontend

---
+++ src/frontend/index.ts
@@ -0,0 +1,43 @@
+const page = document.querySelector<HTMLBodyElement>('.page');
+const pageTitle: HTMLHeadingElement = document.createElement('h1');
+const pageContent: HTMLDivElement = document.createElement('div');
+
+const serverUrl: string = 'http://localhost:3000/';
+
+type PageData = {
+	title: string;
+	content: string;
+};
+
+if (!page) {
+	throw new Error('content not found');
+}
+
+page.appendChild(pageTitle);
+page.appendChild(pageContent);
+
+pageContent.addEventListener('click', (event: MouseEvent) => {
+	event.preventDefault();
+	const target = event.target as HTMLAnchorElement;
+
+	if (target.classList.contains('link')) {
+		const link = target.href;
+		const pageName = link.substring(serverUrl.length);
+		
+		pageDataRequest(pageName)
+			.then((data) => renderPage(data));
+	}
+});
+
+function pageDataRequest(pageName: string) {
+	return fetch(serverUrl + pageName).then((response) => response.json());
+}
+
+function renderPage(data: PageData): void {
+	pageTitle.textContent = data.title;
+	pageContent.innerHTML = data.content;
+}
+
+pageDataRequest('').then((data) => {
+	renderPage(data);
+});
